2afe16a3ed741276d2ef152c194c1318a7f47b2d
SagiSan <samir.sagi@gmail.com>
2017-02-12 14:45:28 +0100

    first-commit

---
+++ ACM.BL/Customer.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ACM.BL
+{
+    public class Customer
+    {
+        private string _lastName;
+        public string LastName
+        {
+            get
+            { return _lastName; }
+            set { _lastName = value; }
+        }
+        public string FirstName { get; set; }
+        public string EmailAdress { get; set; }
+        public int CustomerId { get; private set; }
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+    }
+}
